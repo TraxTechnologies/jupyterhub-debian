97c97903375cde7527123aec38b7b5de0e0b11d5
Daniel Wozniak <daniel.wozniak@traxtech.com>
2017-01-04 04:01:15 +0000

    Initial commit

---
+++ README.txt
@@ -0,0 +1,5 @@
+Debian Package for Jupyterhub
+
+https://github.com/jupyterhub/jupyterhub
+https://github.com/jupyterhub/oauthenticator
+https://github.com/spotify/dh-virtualenv
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='jupyterhub',
+      version='0.7.1',
+      description='Packaged jupyterhub for Trax',
+      author='Devops',
+      author_email='devops@traxtech.com',
+      url='https://github.com/TraxTechnologies/jupyterhub-dhpython',
+      )
