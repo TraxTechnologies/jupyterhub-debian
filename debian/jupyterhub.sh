9b051d688538d90c03beb85ad99f3014c693efc1
Daniel Wozniak <daniel.wozniak@traxtech.com>
2017-01-04 06:24:04 +0000

    Fixup package scripts
    - Add /var/lib/jupyterhub
    - Validate / adjust ownership
    - Fix script proxy to run from virtualenv root and the jupyterhub user's
      home

--- debian/jupyterhub.sh
+++ debian/jupyterhub.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-JUPYTERHUB_CONF=/etc/jupyterhub
 JUPYTERHUB_ROOT=/usr/share/python/jupyterhub
-source $JUPYTERHUB_ROOT/bin/activate
-PATH="$JUPYTERHUB_ROOT/node_modules/configurable-http-proxy/bin/:${PATH}" \
-	JUPYTERHUB_CONF=$JUPYTERHUB_CONF \
-	$JUPYTERHUB_ROOT/bin/jupyterhub $@
+PATH="$JUPYTERHUB_ROOT/node_modules/configurable-http-proxy/bin/:${PATH}"
+(cd $JUPYTERHUB_ROOT && source bin/activate && bin/jupyterhub $@)
